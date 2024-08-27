c17e2e7e2510a8d9fd27ac3f1d6d33356a722e2a
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-08-27 21:46:04 +0530

    Added signup and signin routes for frontend

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { Heading } from "../components/Heading"
+import { Heading } from "../components/Heading";
+import { SubHeading} from "../components/SubHeading";
+import { InputBox } from "../components/InputBox";
+import { Button } from "../components/Button";
+import {BottomWarning} from "../components/BottomWarning";
+
 
 export const Signup =()=>{
     return <div className="bg-slate-300 h-screen flex justify-center"> 
         <div className="flex flex-col justify-center">
           <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
            <Heading label={"Sign up"}/>
+           <SubHeading label={"Enter your information to create your account"}/>
+           <InputBox  placeHolder="John" label={"First Name"}/>
+           <InputBox  placeHolder="Doe" label={"Last Name"}/>
+           <InputBox  placeHolder="aish123@gmail.com" label={"Email"}/>
+           <InputBox  placeHolder="123456" label={"Password"}/>
+           <div className="pt-4">
+              <Button label={"Signup"}/>
+           </div>
+           <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"}/>
           </div>
         </div>
     </div>
