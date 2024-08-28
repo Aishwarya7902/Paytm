d63157f44e59f09a9fab832ec151caaba863be85
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-08-28 22:17:38 +0530

    Adding State Variables In Signup Page

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,3 +1,4 @@
+import { Button } from "./Button"
 export const Users = () => {
     return <>
         <div className="font-bold mt-6 text-lg">
@@ -6,5 +7,14 @@ export const Users = () => {
         <div className="my-2">
             <input type="text" placeholder="Search Users..." className="w-full px-2 py-1 border rounded border-slate-200" />
         </div>
+
+        <div className="flex items-center space-x-4">
+            <div className="w-12 h-12 rounded-full bg-gray-200 flex items-center justify-center">
+                <span className="text-2xl text-black">H</span>
+            </div>
+            <h3 className="text-2xl font-normal">Harkirat Singh</h3>
+        </div>
+
+        
     </>
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,23 +1,42 @@
-import React from "react";
+import { useState } from "react";
 import { Heading } from "../components/Heading";
 import { SubHeading} from "../components/SubHeading";
 import { InputBox } from "../components/InputBox";
 import { Button } from "../components/Button";
 import {BottomWarning} from "../components/BottomWarning";
-
+import axios from "axios";
 
 export const Signup =()=>{
+    const [firstName,setFirstName]=useState("");
+    const [lastName,setLastName]=useState("");
+    const [username,setUserName]=useState("");
+    const [password,setPassword]=useState("");
     return <div className="bg-slate-300 h-screen flex justify-center"> 
         <div className="flex flex-col justify-center">
           <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
            <Heading label={"Sign up"}/>
            <SubHeading label={"Enter your information to create your account"}/>
-           <InputBox  placeHolder="John" label={"First Name"}/>
-           <InputBox  placeHolder="Doe" label={"Last Name"}/>
-           <InputBox  placeHolder="aish123@gmail.com" label={"Email"}/>
-           <InputBox  placeHolder="123456" label={"Password"}/>
+           <InputBox  placeHolder="John" label={"First Name"} onChange={(e)=>{
+            setFirstName(e.target.value)
+           }}/>
+           <InputBox  placeHolder="Doe" label={"Last Name"} onChange={(e)=>{
+            setLastName(e.target.value)
+           }}/>
+           <InputBox  placeHolder="aish123@gmail.com" label={"Email"} onChange={(e)=>{
+            setUserName(e.target.value)
+           }}/>
+           <InputBox  placeHolder="123456" label={"Password"} onChange={(e)=>{
+            setPassword(e.target.value)
+           }}/>
            <div className="pt-4">
-              <Button label={"Signup"}/>
+              <Button label={"Signup"} onClick={()=>{
+                axios.post("http://localhost:3000/api/v1/user/signup",{
+                    username,
+                    firstName,
+                    lastName,
+                    password
+                })
+              }}/>
            </div>
            <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"}/>
           </div>
