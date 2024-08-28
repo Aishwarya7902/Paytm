73d74a1a8d2825395133ec86184cc517c7cc04cc
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-08-28 17:05:19 +0530

    added dashboard and send routes to frontend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Appbar } from "../components/AppBar";
+import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
 
 export const Dashboard = ()=>{
     return <div>
-        Dashboard Page
+        <Appbar/>
+        <div className="m-8">
+            <Balance value={"10,000"}/>
+            <Users/>
+        </div>
     </div>
 }
 
