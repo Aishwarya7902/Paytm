5c3941ba68de33bfbb746b8dde84cf8bcbb8e00a
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-09-03 01:09:02 +0530

    Added functionality to dashboard page

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -4,19 +4,45 @@ import { SubHeading } from "../components/SubHeading";
 import { InputBox } from "../components/InputBox";
 import { Button } from "../components/Button";
 import { BottomWarning } from "../components/BottomWarning";
+import { useState } from "react";
+import axios from "axios"
 
 
 export const Signin = () => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
   return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
         <Heading label={"Sign In"} />
         <SubHeading label={"Enter your credentials to access your account"} />
-        <InputBox placeHolder="aish123@gmail.com" label={"Email"} />
-        <InputBox placeHolder="123456" label={"Password"} />
+
+        <InputBox 
+            onChange={(e) => {
+            setUserName(e.target.value)}} 
+            placeHolder="aish123@gmail.com" 
+            label={"Email"} 
+        />
+
+        <InputBox
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}  
+            placeHolder="123456" 
+            label={"Password"} 
+         />
+
         <div className="pt-4">
-          <Button label={"Sign in"}  />
+          <Button
+            onClick={(e)=>{
+              axios.post("http://localhost:3000/api/v1/user/signin",{
+                  username,
+                  password
+              })
+            }} 
+            label={"Sign in"}  
+          />
         </div>
         <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
       </div>
