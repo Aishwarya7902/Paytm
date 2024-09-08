ebfc45cc9761a40aeb7c29bfadb47bc519b1c401
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-09-08 23:53:06 +0530

    test1

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,12 +4,12 @@ import { Balance } from "../components/Balance";
 import { Users } from "../components/Users";
 
 export const Dashboard = ()=>{
-    return <div>
+    return <>
         <Appbar/>
         <div className="m-8">
             <Balance />
             <Users/>
         </div>
-    </div>
+    </>
 }
 
